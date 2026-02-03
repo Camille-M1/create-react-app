8d96d469cbd51cdcb1f20f6a8258bfb89db56d99
Samira Ali <samiraali@Samiras-MacBook-Air-3.local>
2026-02-03 14:03:49 -0800

    added glassmorphism buttons and logo branding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './logo.png'
 function App() {
   return (
     <div className="App">
+      
+      <nav className="navbar">
+        <div className="logo">Task<span>Pilot</span></div>
+        <div className="logo-container">
+          <img src={logo} alt="TaskPilot Logo" className="nav-logo" />
+         
+        </div>
+        <div className="nav-links">
+          <button className="btn-login">LOG IN</button>
+        </div>
+      </nav>
+
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="hero-content">
+          <h1>Your ultimate task management <span>solution.</span></h1>
+          <p>Streamline your workflow, crush your goals, and navigate your day with TaskPilot.</p>
+          <div className="button-group">
+  <button className="btn-primary">GET STARTED</button>
+  <button className="btn-login-main">LOG IN</button>
+</div>
+        </div>
       </header>
+
+      <section className="features">
+        <div className="feature-card">
+          <h3>Smart Sorting</h3>
+          <p>The Smart Task & Team Management System is a web-based application designed to help teams organize tasks, manage deadlines, assign roles, and track progress. </p>
+        </div>
+        <div className="feature-card">
+          <h3>Cloud Sync</h3>
+          <p>Access your tasks from your MacBook or on the go.</p>
+        </div>
+      </section>
     </div>
   );
 }
